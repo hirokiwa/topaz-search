7247ce7d79c2fb11ad21e43a2d9201bbc3317e85
hirokiwa <hirokifsys@gmail.com>
2024-03-22 20:00:14 +0900

    Create search

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,41 @@
 import './style.css'
-import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-  </div>
-`
+const handleSearch = () => {
+  const nameText = selectQuery.input("#nameText");
+  const inputValue = nameText?.value ?? "";
+  const url = isEmptyString(inputValue) ? undefined : createUrl(inputValue);
+  url && openUrl(url);
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const isEmptyString = (input: string) => input.trim() === "";
+const createUrl = (searchWord: string) => `https://www.google.com/search?q=${searchWord}+site%3Atopaz.dev`;
+
+const selectQuery = {
+  input: (selector: string) => {
+    try {
+      return document.querySelector<HTMLInputElement>(selector);
+    } catch (e) {
+      console.error(e, "Faild to select query");
+      return null;
+    }
+  },
+  button: (selector: string) => {
+    try {
+      return document.querySelector<HTMLButtonElement>(selector);
+    } catch (e) {
+      console.error(e, "Faild to select query");
+      return null;
+    }
+  },
+}
+
+const openUrl = (url: string) => {
+  try {
+    window.location.href = url;
+  } catch(e) {
+    console.error(e, "Faild to select query");
+  }
+}
+
+const checkButton = selectQuery.button('#checkButton');
+checkButton?.addEventListener('click', handleSearch);
