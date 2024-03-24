f4f4b558c2b4aeffb76820cce9b8b8134419324e
hirokiwa <hirokifsys@gmail.com>
2024-03-25 01:40:00 +0900

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        404: resolve(__dirname, '404.html'),
+      }
+    }
+  }
+});
