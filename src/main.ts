d780143e0004716e0717c5318ebf3e5466a6276c
hirokiwa <hirokifsys@gmail.com>
2024-03-25 18:11:52 +0900

    Create button to clear search word

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,14 @@ export const createUrl = (input: string) => {
 }
 
 const selectQuery = {
+  div: (selector: string) => {
+    try {
+      return document.querySelector<HTMLDivElement>(selector) ?? undefined;
+    } catch (e) {
+      console.error(e, "Faild to select query");
+      return undefined;
+    }
+  },
   input: (selector: string) => {
     try {
       return document.querySelector<HTMLInputElement>(selector) ?? undefined;
@@ -44,5 +52,35 @@ export const openUrl = (url: string) => {
   }
 };
 
+const handleClickClearSearchButton = () => {
+  handleActiveClearButton.unactive();
+  inputElement?.focus();
+};
+
+const handleActiveClearButton = {
+  active: () => {
+    if(clearSearchButtonWrapper){
+      clearSearchButton?.addEventListener('click', handleClickClearSearchButton);
+      clearSearchButtonWrapper.style.display = "flex";
+    };
+  },
+  unactive: () => {
+    if(clearSearchButtonWrapper){
+      clearSearchButton?.removeEventListener('click', handleClickClearSearchButton);
+      clearSearchButtonWrapper.style.display = "none";
+    };
+  },
+};
+
+const handleInputSearchWord = (event: Event) => {
+  const target = event.target instanceof HTMLInputElement ? event.target : undefined;
+  const activadeButton = target && target.value !== "";
+  activadeButton ? handleActiveClearButton.active() : handleActiveClearButton.unactive();
+};
+
+const clearSearchButtonWrapper = selectQuery.div("#clearSearchButtonWrapper");
+const clearSearchButton = selectQuery.input("#clearSearchButton");
 const inputElement = selectQuery.input("#topazSearchWord");
+
 inputElement?.addEventListener("keypress", (e: KeyboardEvent) => { e.key === "Enter" && handleSearch()});
+inputElement?.addEventListener('input', handleInputSearchWord);
