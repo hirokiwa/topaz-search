47dbcee4e1653904010d693b9271d2815eb7a8d1
hirokiwa <hirokifsys@gmail.com>
2024-03-29 02:34:27 +0900

    Use vite-pwa

--- src/main.ts
+++ src/main.ts
@@ -165,13 +165,3 @@ if(currentInput && searchInputElement){
 };
 
 bodyElement?.addEventListener("keydown", handleKeyDownDoby);
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').then((registration) => {
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, (err) => {
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  }, {once: true});
-};
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   build: {
@@ -9,5 +10,41 @@ export default defineConfig({
         404: resolve(__dirname, '404.html'),
       }
     }
-  }
+  },
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      
+      manifest: {
+        short_name: "Topa'z 検索くん",
+        name: "Topa'z 検索くん",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "./image/topaz-search-icon-192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "./image/topaz-search-icon-512.png",
+            sizes: "512x512",
+            type: "image/png"
+          },
+          {
+            src: "./image/topaz-search-icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable"
+          }
+        ],
+        background_color: "#ffffff",
+        theme_color: "#000000",
+        lang: "ja",
+      }
+    })
+  ],
 });
