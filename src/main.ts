b2c56fb32d1e1a941d52da1c0ce26cdb51a863f5
hirokiwa <hirokifsys@gmail.com>
2024-03-29 02:40:17 +0900

    Revert "Use vite-pwa"
    This reverts commit 47dbcee4e1653904010d693b9271d2815eb7a8d1.

--- src/main.ts
+++ src/main.ts
@@ -165,3 +165,13 @@ if(currentInput && searchInputElement){
 };
 
 bodyElement?.addEventListener("keydown", handleKeyDownDoby);
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').then((registration) => {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, (err) => {
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  }, {once: true});
+};
